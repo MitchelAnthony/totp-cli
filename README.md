f7035adf76e3b22ef78fbf90df6a27b5fefec318
Mitchel Vroege <mitchel.vroege@2066d.nl>
2022-09-23 15:09:48 +0200

    feat: Add cli option to specify an otpauth url instead of just the secret

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ## Testing
 Generate QR for testing: https://stefansundin.github.io/2fa-qr/
 
-Or use this secret `7NY4BTFIYHYGT6OGSAP3XG4VAV7NOGA2` or this otpauth link `otpauth://totp/John%20Doe?secret=7NY4BTFIYHYGT6OGSAP3XG4VAV7NOGA2&issuer=Acme%20Corp`
+Or use this secret `7NY4BTFIYHYGT6OGSAP3XG4VAV7NOGA2` or this otpauth url `otpauth://totp/John%20Doe?secret=7NY4BTFIYHYGT6OGSAP3XG4VAV7NOGA2&issuer=Acme%20Corp`
 
 ## How to use
 cargo run -- --secret={secret}
+cargo run -- --auth-url="{url}"
+
+Or build the binary `cargo build --release` and then `./target/release/totp --secret=={secret}`
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
+use anyhow::bail;
 use base32::Alphabet;
-// use anyhow::Context;
 use clap::Parser;
 use totp::calculate_totp;
+use url::Url;
 
 #[derive(Debug, Parser)]
 struct Cli {
-    #[clap(long)]
-    secret: String,
+    /// A base32 encoded TOTP secret string
+    #[clap(short, long)]
+    secret: Option<String>,
+
+    /// An otpauth url with a base32 encoded TOTP secret string
+    #[clap(short, long)]
+    auth_url: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
-    // Assume the secret is base32 encoded
-    let secret = base32::decode(Alphabet::RFC4648 { padding: false }, &args.secret).unwrap();
+    let secret: Vec<u8>;
+    if let Some(arg_secret) = args.secret {
+        secret = base32::decode(Alphabet::RFC4648 { padding: false }, &arg_secret).unwrap();
+    } else if let Some(auth_url) = args.auth_url {
+        let otp_auth_url = Url::parse(&auth_url)?;
+        let mut query_params = otp_auth_url.query_pairs();
+        let otp_secret = query_params
+            .find(|(key, _)| key == "secret")
+            .unwrap()
+            .1
+            .clone();
+
+        secret = base32::decode(Alphabet::RFC4648 { padding: false }, &otp_secret).unwrap();
+    } else {
+        bail!("Either --secret or --auth-url must be given.");
+    }
 
     println!("{}", calculate_totp(&secret));
 
